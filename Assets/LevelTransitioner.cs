0d0560764386bba5f8bd630bd9b0e2b13a188974
daniawf <116633286+daniawf@users.noreply.github.com>
2023-11-16 01:07:11 +0800

    Update LevelTransitioner.cs

--- Assets/LevelTransitioner.cs
+++ Assets/LevelTransitioner.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelTransitioner : MonoBehaviour
 {
+    public bool _isLastLevel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,26 @@ public class LevelTransitioner : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D playerCol)
+    {
+        if(_isLastLevel) //win if on last level
+        {
+            SceneManager.LoadScene("WinCutscene");
+        }else
+        {
+            //run fade to black animation (optional)
+
+            //remove all current wave/level of enemies and obstacles
+            GameObject[] waves = GameObject.FindGameObjectsWithTag("Wave");
+            for(int i = 0; i < waves.Length; i++)
+            {
+                Destroy(waves[i]);
+            }
+            //spawn new level prefab
+
+            //make player come in from opposite screen
+        }
     }
 }
